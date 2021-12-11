dd992e47f2e44f5cc369eab70723b280c52f94f2
jorislimonier <joris.limonier@hotmail.fr>
2021-12-11 18:47:49 +0100

    make initial labels

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
+import plotly.express as px
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("/home/joris/Downloads/Export_Mouvements_Cpte courant Green Code 18-30 Study.csv", header=3, sep=";")
+data = pd.read_csv("expenses.csv", header=3, sep=";")
 data["Montant"] = data["Montant"].str.replace(",", ".")
 data["Montant"] = data["Montant"].astype(float)
 data = data.convert_dtypes()
 data["Date d\'opération"] = pd.to_datetime(data["Date d\'opération"])
 
 
-data.dtypes
+data.tail()
 
 
 # %%
-debit = data[np.all([data["Montant"] < 0, data["Type de mouvement"] != "DECOMPTE VISA"], axis=0)].copy()
+debit = data[np.all([data["Montant"] < 0, data["Type de mouvement"]
+                    != "DECOMPTE VISA"], axis=0)].copy()
 debit["spent"] = debit.Montant * -1
 debit.drop(columns="Montant", inplace=True, errors="ignore")
+debit = debit.reset_index(drop=True)
+debit
 
-debit["Type de mouvement"].unique()
-debit.head()
 
-
+# %%
+for row in debit.iterrows():
+    print(type(row))
 # %%
 exp = pd.DataFrame(
     columns=["spent"],
@@ -31,106 +35,111 @@ exp = pd.DataFrame(
            "health", "tech", "clothes", "cash", "gas", "gift", "taxes and utilities", "phone", "rent", "other"]
 )
 exp["spent"] = 0
+debit["label"] = np.nan
 
-# categorize your expenses
-for row in debit.iterrows():
-    info = row[1]
-    comm_lower = info["Communication"].lower()
+
+def categorize(comm):
+    "categorize expenses based on communication"
+    try:
+        comm_lower = comm.lower()
+    except Exception as e:
+        return np.nan
+
     if "escota vinci" in comm_lower:
-        exp.loc["tolls", "spent"] += info["spent"]
+        return "tolls"
     elif "aprr" in comm_lower:
-        exp.loc["tolls", "spent"] += info["spent"]
+        return "tolls"
 
     elif "helios crou 1 sc2341529" in comm_lower:
-        exp.loc["crous_food", "spent"] += info["spent"]
+        return "crous_food"
 
     elif "pharmacie" in comm_lower:
-        exp.loc["health", "spent"] += info["spent"]
+        return "health"
 
     elif "maxicoffee sud" in comm_lower:
-        exp.loc["food_non_groceries", "spent"] += info["spent"]
+        return "food_non_groceries"
     elif "pizza" in comm_lower:
-        exp.loc["food_non_groceries", "spent"] += info["spent"]
+        return "food_non_groceries"
     elif "pizzeria" in comm_lower:
-        exp.loc["food_non_groceries", "spent"] += info["spent"]
+        return "food_non_groceries"
     elif "mirage sarl 4173455" in comm_lower:  # hop store (bar)
-        exp.loc["food_non_groceries", "spent"] += info["spent"]
+        return "food_non_groceries"
     elif "rpa sta acro3" in comm_lower:  # beer with generationZ
-        exp.loc["food_non_groceries", "spent"] += info["spent"]
+        return "food_non_groceries"
     elif "colgan, antibes" in comm_lower:  # The Duke bar
-        exp.loc["food_non_groceries", "spent"] += info["spent"]
+        return "food_non_groceries"
 
     elif "carrefourmarket, mougins" in comm_lower:
-        exp.loc["groceries", "spent"] += info["spent"]
+        return "groceries"
     elif "leader, antibes" in comm_lower:
-        exp.loc["groceries", "spent"] += info["spent"]
+        return "groceries"
     elif "sm casino cs898, biot" in comm_lower:
-        exp.loc["groceries", "spent"] += info["spent"]
+        return "groceries"
     elif "carrefour niceli" in comm_lower:
-        exp.loc["groceries", "spent"] += info["spent"]
+        return "groceries"
     elif "sc baghera, 06mougins" in comm_lower:
-        exp.loc["groceries", "spent"] += info["spent"]
+        return "groceries"
     elif "sm casino cs638" in comm_lower:
-        exp.loc["groceries", "spent"] += info["spent"]
+        return "groceries"
 
     elif "boulanger, mandelieu" in comm_lower:
-        exp.loc["tech", "spent"] += info["spent"]
+        return "tech"
     elif "darty" in comm_lower:
-        exp.loc["tech", "spent"] += info["spent"]
+        return "tech"
 
     elif "retrait bancomat" in comm_lower:
-        exp.loc["cash", "spent"] += info["spent"]
+        return "cash"
 
     elif "esso moulieres" in comm_lower:
-        exp.loc["gas", "spent"] += info["spent"]
+        return "gas"
     elif "q8 martinelli" in comm_lower:
-        exp.loc["gas", "spent"] += info["spent"]
+        return "gas"
     elif "station u 9240101, 06plascassier" in comm_lower:
-        exp.loc["gas", "spent"] += info["spent"]
+        return "gas"
     elif "intermarche dac, gattieres, fra, achat vpay du 09/10/2021 a 08:26" in comm_lower:
-        exp.loc["gas", "spent"] += info["spent"]
+        return "gas"
     elif "tankstelle" in comm_lower:
-        exp.loc["gas", "spent"] += info["spent"]
+        return "gas"
 
     elif "intermarche, cuers" in comm_lower:
-        exp.loc["gift", "spent"] += info["spent"]
+        return "gift"
 
     elif "kiabi" in comm_lower:
-        exp.loc["clothes", "spent"] += info["spent"]
+        return "clothes"
     elif "chaussea" in comm_lower:
-        exp.loc["clothes", "spent"] += info["spent"]
+        return "clothes"
     elif "decathlon" in comm_lower:
-        exp.loc["clothes", "spent"] += info["spent"]
+        return "clothes"
     elif "jules" in comm_lower and "2980914" in comm_lower:
-        exp.loc["clothes", "spent"] += info["spent"]
+        return "clothes"
 
     elif "ville de luxembourg" in comm_lower:
-        exp.loc["taxes and utilities", "spent"] += info["spent"]
+        return "taxes and utilities"
     elif "edf" in comm_lower:
-        exp.loc["taxes and utilities", "spent"] += info["spent"]
+        return "taxes and utilities"
     elif "foyer assurances sa" in comm_lower:
-        exp.loc["taxes and utilities", "spent"] += info["spent"]
+        return "taxes and utilities"
     elif "tdo, 3 rue jean piret" in comm_lower:
-        exp.loc["taxes and utilities", "spent"] += info["spent"]
+        return "taxes and utilities"
 
     elif "sfr" in comm_lower:
-        exp.loc["phone", "spent"] += info["spent"]
+        return "phone"
     elif "orange communications luxembourg" in comm_lower:
-        exp.loc["phone", "spent"] += info["spent"]
+        return "phone"
 
     elif "billaudel" in comm_lower:
-        exp.loc["rent", "spent"] += info["spent"]
+        return "rent"
 
     else:
-        print(f"""{info["Communication"]}\n{info["spent"]}\n""")
-        exp.loc["other", "spent"] += info["spent"]
+        return np.nan
 
-exp
 
+debit["label"] = debit["Communication"].apply(categorize)
+debit["label"].value_counts(dropna=False)
 
 # %%
-import plotly.express as px
-px.bar(exp.sort_values("spent", ascending=False), y="spent", color="spent", color_continuous_scale="Bluered")
+px.bar(exp.sort_values("spent", ascending=False), y="spent",
+       color="spent", color_continuous_scale="Bluered")
 
 
 # %%
@@ -139,6 +148,3 @@ px.scatter(debit, x="Date d'opération", y="spent")
 
 
 # %%
-
-
-
