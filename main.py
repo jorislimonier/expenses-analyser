2ff15003e3bf51fff1ac040b391d1ca50eecb1f5
jorislimonier <joris.limonier@hotmail.fr>
2021-12-12 10:43:47 +0100

    try decision tree and random forest

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 # %%
+from scipy.sparse.construct import rand
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.svm import LinearSVC
-from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
 import plotly.express as px
 import numpy as np
 import pandas as pd
 import importlib
 import data_loader
+import matplotlib.pyplot as plt
 # %%
 
 importlib.reload(data_loader)
 dl = data_loader.DataLoader()
 
-dl.label_encode_debit()
+# dl.label_encode_debit()
 dl.split_date()
 
 # %%
@@ -24,6 +29,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     shuffle=True,
                                                     random_state=42)
 
+# %% [markdown]
+# ## LinearSVC
 # %%
 clf = LinearSVC()
 clf.fit(X_train, y_train)
@@ -31,3 +38,39 @@ y_pred = clf.predict(X_test)
 accuracy_score(y_test, y_pred)
 # %%
 X_test.join(pd.DataFrame({"pred": y_pred}))
+# %% [markdown]
+# ## Decision Tree
+# %%
+df_dummy = pd.get_dummies(dl.debit, columns=["transfer_type", "label"], drop_first=True)
+
+label_col = [col for col in df_dummy.columns if col.startswith("label")]
+y = df_dummy[label_col]
+X = df_dummy.drop(columns=label_col+["communication"])
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    train_size=.8,
+                                                    shuffle=True,
+                                                    random_state=42)
+clf = DecisionTreeClassifier(random_state=4)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+accuracy_score(y_test, y_pred)
+
+plt.figure(figsize=(64,24))
+plot_tree(clf, fontsize=6)
+plt.savefig("tree", dpi=100)
+# %% [markdown] 
+# ## Random Forest
+
+# %%
+clf = RandomForestClassifier()
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+accuracy_score(y_test, y_pred)
+
+# %%
+y_test.sum(axis=1)
+y_test.idxmax(axis=1)
+# %%
+sum(pd.DataFrame(y_pred).idxmax(axis=1).apply(lambda x: label_col[x]).values == y_test.idxmax(axis=1)) / 42
+# %%
+pd.DataFrame(y_pred).idxmax(axis=1).apply(lambda x: label_col[x]).values == y_test.idxmax(axis=1)
